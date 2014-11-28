29cbb2fcb8dec35f4398c5ddc47e82cc6bb7f1c0
jefffhaynes <feedyurhed@gmail.com>
2014-11-28 12:38:56 -0500

    missing files

---
+++ XBee/RemoteAtCommandResponseFrame.cs
@@ -0,0 +1,11 @@
+﻿namespace XBee
+{
+    public class RemoteAtCommandResponseFrame : CommandResponseFrameContent
+    {
+        public LongAddress LongAddress { get; set; }
+
+        public ShortAddress ShortAddress { get; set; }
+
+        public AtCommandResponseFrameContent Content { get; set; }
+    }
+}
