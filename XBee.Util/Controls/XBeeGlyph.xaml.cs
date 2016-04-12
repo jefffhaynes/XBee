00906753fb5047ef404896c6bc323f6a7066fa39
jefffhaynes <feedyurhed@gmail.com>
2016-04-12 00:00:51 -0400

    Revert "Revert "initial util commit""
    This reverts commit 3489689d483afa6b727beb50d5178b00241b7873.

---
+++ XBee.Util/Controls/XBeeGlyph.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace XBee.Util.Controls
+{
+    /// <summary>
+    /// Interaction logic for XBeeGlyph.xaml
+    /// </summary>
+    public partial class XBeeGlyph : UserControl
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text", typeof (string), typeof (XBeeGlyph), new PropertyMetadata(default(string), PropertyChangedCallback));
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var control = (XBeeGlyph)dependencyObject;
+            control.XBeeTextBlock.Text = (string)dependencyPropertyChangedEventArgs.NewValue;
+        }
+
+        public string Text
+        {
+            get { return (string) GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public XBeeGlyph()
+        {
+            InitializeComponent();
+        }
+    }
+}
