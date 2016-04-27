2ad1da186d013f4d426ec04b179fa6ae5d8b8945
jefffhaynes <feedyurhed@gmail.com>
2016-04-26 22:46:48 -0400

    beginning of view model

---
+++ XBee.Util/ViewModels/Coordinator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace XBee.Util.ViewModels
+{
+    public class Coordinator : NodeBase
+    {
+        public Coordinator(XBeeController controller)
+        {
+            Controller = controller;
+            controller.NodeDiscovered += NodeDiscovered;
+            DiscoverCommand = new RelayCommand(Discover);
+        }
+
+        private void NodeDiscovered(object sender, NodeDiscoveredEventArgs e)
+        {
+            var existingNode = EndPoints.SingleOrDefault(node => node.Address == e.Node.Address);
+            if (existingNode != null)
+            {
+                existingNode.Id = e.Name;
+            }
+            else
+            {
+                var node = new EndPoint
+                {
+                    Address = e.Node.Address,
+                    Id = e.Name
+                };
+
+                EndPoints.Add(node);
+            }
+        }
+
+        public XBeeController Controller { get; }
+
+        public ObservableCollection<EndPoint> EndPoints { get; set; } 
+
+        public ICommand DiscoverCommand { get; }
+
+        private async void Discover()
+        {
+            await Controller.DiscoverNetwork();
+        }
+    }
+}
---
+++ XBee.Util/ViewModels/NodeBase.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using XBee.Util.Annotations;
+
+namespace XBee.Util.ViewModels
+{
+    public abstract class NodeBase : INotifyPropertyChanged
+    {
+        private string _id;
+        private NodeAddress _address;
+
+        public string Id
+        {
+            get { return _id; }
+            set
+            {
+                if (value == _id) return;
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public NodeAddress Address
+        {
+            get { return _address; }
+            set
+            {
+                if (Equals(value, _address)) return;
+                _address = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
