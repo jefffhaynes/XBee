c8f2128b876618bb7f1108074cf711caef346d45
jefffhaynes <feedyurhed@gmail.com>
2016-04-26 21:22:50 -0400

    Added comments

--- XBee/Sample.cs
+++ XBee/Sample.cs
@@ -6,6 +6,9 @@ using XBee.Frames;
 
 namespace XBee
 {
+    /// <summary>
+    /// Represents a sample returned from an XBee node.
+    /// </summary>
     public class Sample
     {
         internal Sample(DigitalSampleChannels digitalChannels, DigitalSampleState digitalSampleState, 
@@ -17,12 +20,24 @@ namespace XBee
             AnalogSamples = new ReadOnlyCollection<AnalogSample>(analogSamples.ToList());
         }
 
+        /// <summary>
+        /// Bit-mask indicating which digital channels are included in the sample.
+        /// </summary>
         public DigitalSampleChannels DigitalChannels { get; }
 
+        /// <summary>
+        /// Bit-mask indicating which digital channels are high.
+        /// </summary>
         public DigitalSampleState DigitalSampleState { get; }
 
+        /// <summary>
+        /// Bit-mask indicating which analog channels are included in the sample.
+        /// </summary>
         public AnalogSampleChannels AnalogChannels { get; }
 
+        /// <summary>
+        /// Collection of analog readings with one reading per active analog channel.
+        /// </summary>
         public ReadOnlyCollection<AnalogSample> AnalogSamples { get; }
 
         public override string ToString()
