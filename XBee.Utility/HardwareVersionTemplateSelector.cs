e1187789773cc425a952567415168e2a298a8a8e
jefffhaynes <feedyurhed@gmail.com>
2017-05-29 13:59:03 -0400

    cleanup

--- XBee.Utility/HardwareVersionTemplateSelector.cs
+++ XBee.Utility/HardwareVersionTemplateSelector.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using XBee.Frames.AtCommands;
 using XBee.Utility.ViewModels;
@@ -8,14 +7,13 @@ namespace XBee.Utility
 {
     public class HardwareVersionTemplateSelector : DataTemplateSelector
     {
-        public DataTemplate DefaultDataTemplate { get; set; }
         public DataTemplate CellularDataTemplate { get; set; }
 
         protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
         {
             if (item == null)
             {
-                return DefaultDataTemplate;
+                return null;
             }
 
             var hardware = (IHardware) item;
