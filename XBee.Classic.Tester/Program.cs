6d2f7c6ec9cb06eb6849abae900564bc9acdaaf6
jefffhaynes <feedyurhed@gmail.com>
2017-06-10 16:04:37 -0400

    unified package!

--- XBee.Classic.Tester/Program.cs
+++ XBee.Classic.Tester/Program.cs
@@ -9,9 +9,17 @@ namespace XBee.Classic.Tester
             var controllerTask = XBeeController.FindAndOpenAsync(9600);
             controllerTask.Wait();
             var controller = controllerTask.Result;
+
+            if (controller == null)
+            {
+                return;
+            }
+
+            Console.WriteLine("Found controller.");
             controller.NodeDiscovered += (sender, eventArgs) => Console.WriteLine($"{eventArgs.Name} discovered.");
             var discoverTask = controller.DiscoverNetworkAsync();
             discoverTask.Wait();
+            Console.WriteLine("Discovery finished.");
             Console.ReadKey();
         }
     }
