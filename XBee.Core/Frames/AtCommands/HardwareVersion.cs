01bae4f0ab675f5bda3a4ed2da8122d67fab05a2
Jeff Haynes <feedyurhed@gmail.com>
2017-08-05 11:00:58 -0400

    added enum for XBeePro868

--- XBee.Core/Frames/AtCommands/HardwareVersion.cs
+++ XBee.Core/Frames/AtCommands/HardwareVersion.cs
@@ -7,6 +7,7 @@
         ZNetZigBeeS2 = 0x19,   // XB24_BXIX_XXX
         XBeeProS2 = 0x1A,      // XBP24_BXIX_XXX
         XBeePro900 = 0x1B,     // XBP09_DXIX_XXX
+        XBeePro868 = 0x1D,     // XBP08_DXXX_XXX
         XBeeProS2B = 0x1E,     // XBP24B
         XBeeProS2C = 0x21,     // XBP24C
         XBee24S2C = 0x22,      // XB24C
